1c2eea872ee56afd6ab052d7c45e6bc30772dae6
Kamilfo-Developer <Biktimirov.kamil@yandex.ru>
2022-04-01 22:48:09 +0300

    Added caching of CB data to get rid of "too many requests" error
    - Adeded new a way of caching the data from server to database.
    - Path of DB can be changed in config file
    - Changed the strucure of CBRates class. Now it uses decorators.

--- bot/data_modules/cbrates.py
+++ bot/data_modules/cbrates.py
@@ -1,7 +1,9 @@
 from xml.dom import minidom
 from bot.utils.date import Date
-from datetime import date as dt
+import bot.config as config
 import requests
+import sqlite3
+import json
 
 class CBRates:
     
@@ -9,16 +11,28 @@ class CBRates:
                  month=Date.get_current_month(), 
                  year=Date.get_current_year()) -> None:
         
-        self.date = Date(day, month, year)
-        self.data = self.__get_CB_exchange_rates()
-
-    def __get_CB_exchange_rates(self):
-        date = self.date
+        self.__date = Date(day, month, year).get_next_day_date()
+        self.__data = self.__get_CB_exchange_rates()
         
-        if (dt.today().day == date.day and date.month == dt.today().month and date.year == dt.today().year):
-            req = requests.get("http://www.cbr.ru/scripts/XML_daily.asp")
-        else: 
-            req = requests.get(f"http://www.cbr.ru/scripts/XML_daily.asp?date_req={date.get_formated_date()}")
+    
+    @property
+    def date(self):
+        return self.__date
+    
+    @date.setter
+    def date(self, value):
+        raise AttributeError("date property is not allowed to be changed.")
+    
+    @property
+    def data(self):
+        return self.__data
+    
+    @date.setter
+    def date(self, value):
+        raise AttributeError("data property is not allowed to be changed.")
+
+    def __require_CB_exchange_rates(self, date: Date):
+        req = requests.get(f"http://www.cbr.ru/scripts/XML_daily.asp?date_req={date.get_formated_date()}")
         #https://iss.moex.com/iss/statistics/engines/currency/markets/selt/rates.json?iss.meta=off
         
         if (req.status_code != 200): 
@@ -29,6 +43,8 @@ class CBRates:
         items = parsed.getElementsByTagName("Valute")
 
         result = dict()
+        
+        result["date"] = "-".join(parsed.getElementsByTagName("ValCurs")[0].getAttribute("Date").split('.'))
         
         for valute in items:
             current_valute = valute.childNodes[1].firstChild.nodeValue
@@ -41,13 +57,49 @@ class CBRates:
         
         return result
     
+    def __get_CB_exchange_rates(self):
+        date = self.__date
+        
+        if config.USE_CACHE:
+            
+            con = sqlite3.connect(config.CBRATES_CACHE_DATABASE_PATH)
+            
+            cur = con.cursor()
+            
+            cur.execute("CREATE TABLE IF NOT EXISTS cbcache (date TEXT, data TEXT)")
+            
+            formated_date = date.get_formated_date()
+
+            cache_data = list(cur.execute(f"SELECT data FROM cbcache WHERE date = '{formated_date}' "))
+            
+            if len(cache_data) == 0:
+                data = self.__require_CB_exchange_rates(date)
+                
+                day, month, year = data["date"].split("-")
+
+                
+                if Date(int(day), int(month), int(year)).get_formated_date() != date.get_previous_day_date().get_formated_date():
+                    json_data = json.dumps(data)
+                    
+                    cur.execute(f"INSERT INTO cbcache VALUES ('{formated_date}', '{json_data}')")
+                    
+                    con.commit()
+                    con.close()
+                
+                return data
+                
+            con.close()
+            
+            return json.loads(cache_data[0][0])
+        
+        return self.__require_CB_exchange_rates(date)
+        
     def get_previous_exchange_rates(self):
-        date = self.date
-        current_exchange_rates = self.data
+        date = self.__date
+        current_exchange_rates = self.__data
         
         prev_date = date.get_previous_day_date()
         previous_exchange_rates = CBRates(prev_date.day, prev_date.month, prev_date.year)
-        
         
         while previous_exchange_rates.data == current_exchange_rates:
             previous_exchange_rates = CBRates(prev_date.day, prev_date.month, prev_date.year)
@@ -55,4 +107,5 @@ class CBRates:
     
         return previous_exchange_rates
 
-print(CBRates().data['USD']['price'])
+rates = CBRates()
+    
