ce86a72b9ebee82bb4e342a528b87d2495f1a369
Kamilfo-Developer <Biktimirov.kamil@yandex.ru>
2022-06-05 03:56:24 +0300

    Added documentation for the class methods

--- bot/data_modules/cbrates.py
+++ bot/data_modules/cbrates.py
@@ -9,7 +9,7 @@ import time
 
 class CBRates:
     
-    def __init__(self, date=(Date.get_current_day(), 
+    def __init__(self, date: tuple = (Date.get_current_day(), 
                  Date.get_current_month(), 
                  Date.get_current_year())) -> None:
         try:
@@ -35,7 +35,7 @@ class CBRates:
     def date(self, value):
         raise AttributeError("\"data\" property is not allowed to be changed.")
 
-    def __require_CB_exchange_rates(self, date: Date):
+    def __require_CB_exchange_rates(self, date: Date) -> dict:
         req = requests.get(f"http://www.cbr.ru/scripts/XML_daily.asp?date_req={date.get_formated_date()}")
         #https://iss.moex.com/iss/statistics/engines/currency/markets/selt/rates.json?iss.meta=off
         
@@ -61,16 +61,22 @@ class CBRates:
         
         return result
     
-    def __get_CB_exchange_rates(self):
+    def __get_CB_exchange_rates(self) -> dict:
+        """Gets CB exchange rates.
+        If cache used the method returns data either from the cache database or from the official CB API if more than CACHE_INTERVAL in seconds passed since the last try  
+
+        Returns:
+            dict: the CB rates data
+        """
         date = self.__date
         
-        DB_client = pymongo.MongoClient("mongodb://localhost:27017")
+        if config.USE_CACHE:
+            DB_client = pymongo.MongoClient("mongodb://localhost:27017")
 
-        DB = DB_client["cache"]
+            DB = DB_client["cache"]
 
-        cache = DB["CB_cache"]
-        
-        if config.USE_CACHE:            
+            cache = DB["CB_cache"]        
+            
             formated_date = date.get_formated_date()
             
             cache_data = cache.find_one({"date": formated_date})
@@ -91,12 +97,19 @@ class CBRates:
         return self.__require_CB_exchange_rates(date)
         
     def get_previous_exchange_rates(self):
+        """Returns the previous exchange rates  
+
+        Returns:
+            CBRates: instance of CBRates class that contains previous exchange rates data
+        """
+        
         date = self.__date
         current_exchange_rates = self.__data
         
         prev_date = date.get_previous_day_date()
         previous_exchange_rates = CBRates(date=(prev_date.day, prev_date.month, prev_date.year))
         
+        #Here we check either previous day rates are equal to the current
         while previous_exchange_rates.data == current_exchange_rates:
             prev_date = prev_date.get_previous_day_date()
             previous_exchange_rates = CBRates(date=(prev_date.day, prev_date.month, prev_date.year))
